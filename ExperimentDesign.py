857e15089db84fa770a97551b7f5f75310bc18cd
plpla <pier-luc.plante.1@ulaval.ca>
2020-12-06 17:02:24 -0500

    Update ExperimentDesign.py

--- ExperimentDesign.py
+++ ExperimentDesign.py
@@ -1,20 +1,17 @@
-DATA_MATRIX = "Data\\DataMatrix.csv"
-METADATA = "Data\\sample_metadata_clean.xlsx"
+DATA_MATRIX = "Data\\DataMatrix_excel.xlsx" # Your file
+
+FILE_TYPE = "excel" # File format progenesis, excel, csv or tsv. Check demo files if you are not sure which one to use.
+USE_NORMALIZED = True  # If using progenesis file, otherwise it is not considered
+N_SPLITS = 3 # The number of Monte-Carlo splits to generate for each design
 
 EXPERIMENT_DESIGNS={
-    "All Med-Na":{
+    "All Med vs N-A":{ # Don't use "_" in the exp name.
         "classes": {
             "North-American":["Alimed_A", "Med_A"],
             "Mediteranean": ["Alimed_B", "Med_B", "Med_C"]
         },
         "TestSize": 0.2,
+        "positive_class": "Mediteranean"
     },
-    "Study Med":{
-        "classes": {
-            "North-American":["Med_A"],
-            "Mediteranean": ["Med_B", "Med_C"]
-        },
-        "TestSize": 0.2,
-    }
 }
     
